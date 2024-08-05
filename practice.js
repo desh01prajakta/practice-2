f8753305f75e79a2239cb86900846c0547c85e1c
desh01prajakta <prajudeshmukh11@gmail.com>
2024-08-05 00:24:29 -0500

    initial commit

---
+++ practice.js
@@ -0,0 +1,43 @@
+console.log("helo");
+console.log(5+3);
+
+function displayAlert(){
+alert("hello World")
+}
+
+/*helwosjdishd*/
+//sdsdsd//
+
+
+let a = 10;
+let b = 20;
+let total = a+b;
+console.log(total)
+
+staticVariable = "good";
+
+console.log("staticVariable:", staticVariable);
+
+
+staticVariableAfter = 50;
+
+console.log("staticVariableAfter:", staticVariableAfter);
+
+let name = "javascript";
+let matter = "I love";
+
+console.log(`${name} ${matter}!`)
+
+const sum = 5 + 4;
+
+console.log(typeof sum);
+
+const name1 = "pd";
+console.log(typeof name)
+
+const 
+
+
+
+
+
